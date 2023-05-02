9c86363e3404bd654655c3a2539b32898f7d4849
shai9 <destronan5@gmail.com>
2023-05-02 22:55:11 +0300

    Add components

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,10 +10,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.gstatic.com"  />
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;600&display=swap" rel="stylesheet" />
       </Head>
-      <main>
+      <main className='products-heading'>
         HeroBanner
         <div>
           <h1>The ultimate phone accessory destination</h1>
